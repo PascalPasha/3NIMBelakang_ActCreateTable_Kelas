bb3aedca3697cffb0c858b5fa2b7a06e1753b4f0
Pahlevi Pasha <pahlevi.pasha09@gmail.com>
2022-03-17 14:45:41 +0700

    Add project files.

---
+++ create_tabel/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace create_tabel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Create();
+        }
+
+        public void Create()
+        {
+            SqlConnection con = null;
+            try 
+            {
+                con = new SqlConnection("data source = (Sesuai dengan server name masing - masing) ;" +
+                "database=ProdiTI;Integrated Securty = True");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("Create Table Mahasiswa (NIM Char(12) not null primary key, Nama Varchar(50), Alamat Varchar(50), Jenis_kelamin Varchar(5))", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Create tabel berhasil");
+                Console.ReadKey();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Error", e);
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    }
+
+}
